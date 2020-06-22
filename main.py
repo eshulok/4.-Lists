8fc0f62b22666bd5e3ea211f7cbfe5f0591b6311
Liz S <shulok@berkeley.edu>
2020-06-22 18:19:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Lists are a type of variable to store related pieces of information
+#To create a list, you use square brackets like this [] and use commas inside the brackets to separate items in the list
+
+#You may want to store a player's inventory in a game
+inventory = ["ax", "apple", "hat", "tent"]
+
+#When you print the whole list, it displays it with the brackets
+print (inventory)
+
+#if you just want to print just one item from your list, you specify the index of the item in the list in brackets after the variable 
+#For example, to print the item at index 1 in inventory, run the following line
+print (inventory[1])
+
+#Notice that the line above prints "apple"
+
+#print(my_list)
+
+#num_list = [1,2,3,5]
+#print(num_list)
+
+#print(my_list[-1])
+
+#my_list[1] = 'Pepsi'
+#print(my_list)
+
+#print(my_list[2:4])
